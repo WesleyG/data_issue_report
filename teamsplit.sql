bdb5f824a067517dcbbc43cd993ad10be48691f2
Wes Gillis <wes.gillis@gmail.com>
2014-08-26 00:11:50 +0000

    new sql queries and moved several things to production

--- teamsplit.sql
+++ teamsplit.sql
@@ -1,14 +1,54 @@
-UPDATE btj SET btj.end_date='8-10-2014', btj.k_usr_operator=565, btj.time_stamp='2014-08-21 10:50:55.550' FROM BRK_broker__TEA_team__JOIN btj INNER JOIN brk_broker b on  btj.k_brk_broker = b.pk_brk_broker INNER JOIN tea_team t on t.pk_tea_team = btj.k_tea_team INNER JOIN tea_team_role r on r.pk_tea_team_role = btj.k_tea_team_role WHERE b.cid = 7 AND b.broker_id = '26666' 
+INSERT INTO BRK_broker__TEA_team__JOIN (cid,k_brk_broker,K_TEA_team,start_date,time_stamp,K_USR_operator,k_tea_team_role) SELECT 7, b.pk_brk_broker, 460, '08-11-2014', '2014-08-22 10:50:55.550', 565, 4 FROM brk_broker b WHERE b.broker_id = '26666'
 
 
-INSERT INTO BRK_broker__TEA_team__JOIN
-	(cid
-      ,K_brk_broker
-      ,K_TEA_team
-      ,start_date
-      ,time_stamp
-      ,K_USR_operator
-      ,k_tea_team_role)
-VALUES (7,b.pk_brk_broker,398,'08-11-2014', '2014-08-21 10:50:55.550', 565, 4)
-FROM BRK_broker__TEA_team__JOIN btj
-	INNER JOIN brk_broker b on b.pk_brk_broker = btj.k_brk_broker
+/* data issues added some alter table statements */
+
+USE [hca_broker_prod_soundpath]
+GO
+
+ALTER TABLE TAG_data_issues
+ADD 
+	--k_usr_operator int
+	--k_tea_team int
+	--time_stamp datetime
+	team_period_start datetime
+GO
+
+USE [hca_broker_prod_soundpath]
+GO
+
+ALTER TABLE TAG_issue_status
+ADD 
+	issue_status_id int
+GO
+
+/*
+/* note if you try to do this from hca9 there will be an issue as the db name is both on hca9 and hca6 */
+USE [hca_broker_prod_soundpath]
+GO
+
+/****** Object:  Table [dbo].[tag_issue_comment]    Script Date: 8/26/2014 2:58:13 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[tag_issue_comment](
+	[pk_issue_comment] [int] IDENTITY(1,1) NOT NULL,
+	[k_TAG_data_issues] [int] NULL,
+	[comment] [varchar](max) NULL,
+	[k_USR_operator] [int] NULL,
+	[time_stamp] [datetime] NULL,
+	[user_type] [int] NULL,
+	[k_USR_operator_name] [varchar](50) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+*/
